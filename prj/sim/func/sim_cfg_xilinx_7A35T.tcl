6430f2dc5cb90bf1e1f2b52184cfcb7aade394b4
hz <dxp@ngs.ru>
2018-06-14 13:37:12 +0700

    add initial files

---
+++ prj/sim/func/sim_cfg_xilinx_7A35T.tcl
@@ -0,0 +1,33 @@
+#-------------------------------------------------------------------------------
+#	Project:       Any
+#	Platform:      xilinx_7A35T
+#
+#	Description:   Simple test config
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+set REF_DIR        "../../..";
+set SCRIPT_DIR     "${REF_DIR}/script"
+
+#-------------------------------------------------------------------------------
+set CfgName        "xilinx_7A35T"
+set DesignName     "top_tb"
+
+#set TimeResolution "10ps"
+set WorkLib        "wlib"
+
+#-------------------------------------------------------------------------------
+set Src  [ list                                   \
+	       "${REF_DIR}/prj/src"                   \
+	       "${REF_DIR}/prj/src/slon"              \
+	       "${REF_DIR}/prj/src/slon/slonick"      \
+	     ]
+set Lib  [ list                                   \
+	       "${REF_DIR}/lib"                       \
+	     ]
+set Sim  [ list                                   \
+	       "${REF_DIR}/prj/sim/func/src"          \
+	     ]
+
+#-------------------------------------------------------------------------------
+source ${SCRIPT_DIR}/questa_sim.tcl
---
+++ prj/sim/func/src/top_tb.sv
@@ -0,0 +1,34 @@
+//-------------------------------------------------------------------------------
+//
+//     Project: Any
+//
+//     Purpose: Default testbench file
+//
+//
+//-------------------------------------------------------------------------------
+
+`timescale 1ns/1ps
+
+module top_tb;
+
+logic test = 1;
+logic res;
+
+initial begin
+    for(int i = 0; i < 100; i++) begin
+        #10ns
+        test = ~test;
+    end
+
+    #100ns
+    $stop()
+end
+
+top top_inst
+(
+    .in  ( test ),
+    .out ( res  )
+);
+
+endmodule
+//-------------------------------------------------------------------------------
