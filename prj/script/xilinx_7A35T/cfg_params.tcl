a2057a47e26c453f80067f63e17c5d1e3475a0ad
hz <dxp@ngs.ru>
2018-06-15 18:59:03 +0700

    change project structure (for 7A35T): move build platforms to 'build/<platform>' dir

--- prj/script/xilinx_7A35T/cfg_params.tcl
+++ prj/script/xilinx_7A35T/cfg_params.tcl
@@ -1,22 +1,36 @@
 #--------------------------------------------------------------------------------
-#	Project:       Any
-#	Platform:      xilinx_7A35T
+#       Project:       Any
+#       Platform:      xilinx_7A35T
 #
-#	Description:   Project parameters
+#       Description:   Project parameters
 #--------------------------------------------------------------------------------
 
 #--------------------------------------------------------------------------------
 set USE_PLL           0
 set USE_MEM_IP_CORES  0
 
+set CLK_FACTOR   100_000_000
+set DOUT_WIDTH             4
+set DNUM_WIDTH             4
+
+set TABLE_ID               0
+set DNUM_POLARITY          1
+
+#--------------------------------------------------------------------------------
+puts $prjDefFile [format "`define CLK_FACTOR    %s" $CLK_FACTOR];
+puts $prjDefFile [format "`define DOUT_WIDTH    %s" $DOUT_WIDTH];
+puts $prjDefFile [format "`define DNUM_WIDTH    %s" $DNUM_WIDTH];
+puts $prjDefFile [format "`define TABLE_ID      %s" $TABLE_ID];
+puts $prjDefFile [format "`define DNUM_POLARITY %s" $DNUM_POLARITY];
+
 
 #--------------------------------------------------------------------------------
 if {$USE_PLL == 1} {
-	puts $prjDefFile [format "`define USE_PLL"];
+        puts $prjDefFile [format "`define USE_PLL"];
 }
 
 if {$USE_MEM_IP_CORES == 1} {
-	puts $prjDefFile [format "`define USE_MEM_IP_CORES"];
+        puts $prjDefFile [format "`define USE_MEM_IP_CORES"];
 }
 #--------------------------------------------------------------------------------
 
