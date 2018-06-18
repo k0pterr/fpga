e34dbd6307b5d982af16c735f505addbe35a15b5
hz <dxp@ngs.ru>
2018-06-18 12:08:47 +0700

    fix 7A50T configuration for new project structure

--- prj/cfg/xilinx_7A50T/cfg_params.tcl
+++ prj/cfg/xilinx_7A50T/cfg_params.tcl
@@ -1,11 +1,11 @@
-#-------------------------------------------------------------------------------
-#       project:       slon5_test
-#       cfg:           xilinx_7A50T
+#--------------------------------------------------------------------------------
+#       Project:         Any
+#       Configuration:   xilinx_7A50T
 #
-#       description:   slon5 start-up
-#-------------------------------------------------------------------------------
+#       Description:   Project parameters
+#--------------------------------------------------------------------------------
 
-#-------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------
 set USE_PLL           0
 set USE_MEM_IP_CORES  0
 
@@ -16,14 +16,15 @@ set DNUM_WIDTH             4
 set TABLE_ID               1
 set DNUM_POLARITY          1
 
-#-------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------
 puts $prjDefFile [format "`define CLK_FACTOR    %s" $CLK_FACTOR];
 puts $prjDefFile [format "`define DOUT_WIDTH    %s" $DOUT_WIDTH];
 puts $prjDefFile [format "`define DNUM_WIDTH    %s" $DNUM_WIDTH];
 puts $prjDefFile [format "`define TABLE_ID      %s" $TABLE_ID];
 puts $prjDefFile [format "`define DNUM_POLARITY %s" $DNUM_POLARITY];
 
-#-------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------
 if {$USE_PLL == 1} {
         puts $prjDefFile [format "`define USE_PLL"];
 }
@@ -31,6 +32,7 @@ if {$USE_PLL == 1} {
 if {$USE_MEM_IP_CORES == 1} {
         puts $prjDefFile [format "`define USE_MEM_IP_CORES"];
 }
+#--------------------------------------------------------------------------------
 
 
 
